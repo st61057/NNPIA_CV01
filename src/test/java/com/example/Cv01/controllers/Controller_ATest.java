dad7fc52c81de10733ff1ae2c971e6a4d7652a9c
Zahalka Nicholas <st61057@upce.cz>
2024-02-13 17:51:21 +0100

    hotfix

--- src/test/java/com/example/Cv01/controllers/Controller_ATest.java
+++ src/test/java/com/example/Cv01/controllers/Controller_ATest.java
@@ -1,17 +1,14 @@
 package com.example.Cv01.controllers;
 
-import com.example.Cv01.controllers.Controller_A;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class Controller_ATest {
+
+    @Autowired
     Controller_A controllerA;
-    @BeforeEach
-    void setUp() {
-        controllerA = new Controller_A();
-    }
 
     @Test
     void testControllerA() {
--- src/test/java/com/example/Cv01/controllers/Controller_DTest.java
+++ src/test/java/com/example/Cv01/controllers/Controller_DTest.java
@@ -1,7 +1,5 @@
 package com.example.Cv01.controllers;
 
-import com.example.Cv01.services.Greeting;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
