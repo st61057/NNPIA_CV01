f44bdd0468648b8427ca911b3404f3c59da812df
Zahalka Nicholas <st61057@upce.cz>
2024-02-13 16:41:26 +0100

    blabal

---
+++ src/test/java/com/example/Cv01/Cv01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cv01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cv01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
